90ba87597fa661d75d4792838a7caa529664780a
Catalin Boruga <catalinboruga@yahoo.com>
2017-05-13 16:05:43 +0200

    Use rubocop to improve calculator app

--- calculator.rb
+++ calculator.rb
@@ -32,68 +32,66 @@ end
 
 prompt("Hi #{name}!")
 
-loop do 
-number1 = ''
 loop do
-  prompt("What's the first number")
-  number1 = Kernel.gets().chomp()
+  number1 = ''
+  loop do
+    prompt("What's the first number")
+    number1 = Kernel.gets().chomp()
 
-  if valid_number?(number1)
-    break
-  else
-    prompt("Something is wrong!")
+    if valid_number?(number1)
+      break
+    else
+      prompt("Something is wrong!")
+    end
   end
-end
-
-number2 = ''
-loop do 
-  prompt("What's the second number")
-  number2 = Kernel.gets().chomp()
-
-  if valid_number?(number2)
-    break
-  else
-    prompt("Something is wrong!")
+  number2 = ''
+  loop do
+    prompt("What's the second number")
+    number2 = Kernel.gets().chomp()
+    if valid_number?(number2)
+      break
+    else
+      prompt("Something is wrong!")
+    end
   end
-end
-operator_prompt = <<-MSG
-  What operations would you like to perform?
-  1) add
-  2) substract
-  3) multiply
-  4) divide
-MSG
+  operator_prompt = <<-MSG
+    What operations would you like to perform?
+    1) add
+    2) substract
+    3) multiply
+    4) divide
+  MSG
 
-prompt(operator_prompt)
+  prompt(operator_prompt)
 
-operator = ''
-loop do 
-  operator = Kernel.gets().chomp()
-  if  %w(1 2 3 4).include?(operator)
-    break
-  else
-    prompt("Must choose 1,2,3 or 4")
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+    if %w[1 2 3 4].include?(operator)
+      break
+    else
+      prompt("Must choose 1,2,3 or 4")
+    end
   end
-end
 
-prompt("#{operation_to_message(operator)} the two numbers..")
+  prompt("#{operation_to_message(operator)} the two numbers..")
 
-result = case operator
-         when '1'
-          number1.to_i() + number2.to_i()
-         when '2'
-          number1.to_i() - number2.to_i()
-         when '3'
-          number1.to_i() * number2.to_i()
-         when '4'
-          number1.to_f() * number2.to_f()
-end
+  result = case operator
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() * number2.to_f()
+           end
 
-prompt( "The result is #{result}" )
+  prompt("The result is #{result}")
 
-prompt("Do you want to perform another calculation? (Y to calculate again)")
-answer = Kernel.gets().chomp()
-break unless answer.downcase().start_with?('y') 
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end
 
 prompt("Thanks for your participation!")
